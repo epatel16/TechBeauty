71c2bc46f4041838b8e7fbfb5b1308536cf2fcda
“subinkim” <subinrice@naver.com>
2024-03-13 17:26:41 -0700

    brand and product page implemented

--- webClientInterface/app.py
+++ webClientInterface/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
-import requests
+import sys
 
 # SQLAlchemy for Interactions with MySQL
 from sqlalchemy import create_engine
@@ -61,7 +61,7 @@ def authenticate(username, password) -> bool:
 # execute a query to check if the username exists in the database
 def check_username(username) -> bool:
     # select users who have a matching username
-    if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount != 0:
+    if db.execute(text("SELECT * FROM user_info WHERE username = :username"), {"username": username}).rowcount != 0:
         return 1
     else: 
         return 0
@@ -71,10 +71,15 @@ def check_username(username) -> bool:
 # a list of products, for which we will always return same set of
 # attributes 
 def browse_products(sql = ''):
-    sql = 'SELECT product_id, brand_name, product_name, product_type, rating FROM product \
+    sql = 'SELECT product_id, brand_name, product_name, product_type, price, rating FROM product \
             NATURAL JOIN store NATURAL JOIN brand %s;' % sql
-    db.execute(sql)
-    rows = db.fetchall()
+    rows = db.execute(text(sql)).fetchall()
+    return rows    
+
+# execute a query to get singular product given the query
+def browse_one_products(sql = ''):
+    sql = 'SELECT * FROM product NATURAL JOIN store NATURAL JOIN brand %s;' % sql
+    rows = db.execute(text(sql)).fetchone()
     return rows    
 
 
@@ -83,89 +88,105 @@ def browse_products(sql = ''):
 ########################################
 #Index page where new users can register
 @app.route("/")
+@app.route("/index")
 def index():
-    return redirect(url_for('home'))
-
-@app.route("/home")
-def home():
     return render_template("index.html", login = session.get("logged_in",), username = session.get('username'))
-        
-#Login
+      
+# User attemps to log in
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    ## is the user is already logged in
     if session.get("logged_in"):
-        return redirect(url_for('home'))
+        return redirect(url_for('index'))
+    
+    ## if the request method is GET
     if request.method == "GET":
         return render_template("auth/login.html", login=session.get("logged_in"), username=session.get("username"))
+    
+    ## if the request method is POST
+    # get username and password from the form
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    #Check if the user entered username and password
+    if username is None or password is None:
+        return render_template("auth/login.html", message = "Please fill in all the required fields.", 
+                                login = session.get("logged_in"))
+
+    #Check if credentials are correct
+    if not authenticate(username, password):
+        return render_template("auth/login.html", message="Wrong credentials. Try again.",
+                                login=session.get("logged_in"),username=session.get("username"))
     else:
-        username = request.form.get('username')
-        password = request.form.get('password')
+        session["logged_in"] = True
+        session["username"] = username
+        return redirect(url_for('index'))
 
-        #Check if the user entered username and password
-        if (username is None) or (password is None):
-            return render_template("auth/login.html", message = "Please fill in all the required fields.", 
-                                    login = session.get("logged_in"))
-
-        #Check if credentials are correct
-        if not authenticate(username, password):
-            return render_template("auth/login.html", message="Wrong credentials. Try again.",
-                                    login=session.get("logged_in"),username=session.get("username"))
-        else:
-            return redirect(url_for('home'), login=session.get("logged_in"),username=session.get("username"))
-
-#Logout - clear the session when users log out
+# When user attemps to log out
 @app.route("/log_out")
 def log_out():
+    # reset session variables and flush out session
     session["logged_in"] = False
+    session["username"] = ""
     session.clear()
     return redirect(url_for('index'))
 
-#Register - make an account
+# When user registers for a new account
 @app.route("/register", methods=["POST", "GET"])
 def register():
+    ## if the user is already logged in, take them to home
     if session.get("logged_in"):
-        return redirect(url_for('home'))
+        return redirect(url_for('index'))
+    
+    ## if request method is "GET"
     if request.method == "GET":
         return render_template("auth/register.html", login=session.get("logged_in"), username=session.get("username"))
-    if request.method == "POST":
-        #Get data from the form
-        username = request.form.get('username').lower()
-        password = request.form.get('password')
+    
+    ## if request method is "POST"
+    #Get data from the form
+    username = request.form.get('username').lower()
+    password = request.form.get('password')
 
-        # Check if inputs match the criteria
-        if username is None or password is None:
-            return render_template("auth/register.html", message = "Please fill in all the required fields.", 
-                                   login=session.get("logged_in"), username = session.get("username"))
-        elif len(username) < 6:
-            return render_template("auth/register.html", message="Your username has to be 6 characters or longer.", 
-                                   login = session.get("logged_in"), username = session.get("username"))
-        elif len(password) < 8:
-            return render_template("auth/register.html", message="Your password has to be 8 characters or longer.", 
-                                   login=session.get("logged_in"), username = session.get("username"))
+    # Check if inputs match the criteria
+    if username is None or password is None:
+        return render_template("auth/register.html", message = "Missing required fields", 
+                                login=session.get("logged_in"), username = session.get("username"))
+    elif len(username) < 6 or len(username) > 20:
+        return render_template("auth/register.html", message="Username must be between 6 and 20 characters", 
+                                login = session.get("logged_in"), username = session.get("username"))
+    elif len(password) < 8 or len(password) > 20:
+        return render_template("auth/register.html", message="Password must be between 8 and 20 characters", 
+                                login=session.get("logged_in"), username = session.get("username"))
 
-        # Insert new user into database
-        if check_username(username):
-            return render_template("auth/register.html",  message = "Username already exists :(", 
-                                   login=session.get("logged_in"), username = session.get("username"))
-        else:
-            db.execute("CALL sp_add_users(:username, :pwd)",{"username": username, "pwd":password})
-            db.commit()
-            return render_template("auth/login.html", message = "Please sign in.",
-                                   login = session.get("logged_in"), username = session.get("username"))
+    # Insert new user into database
+    if check_username(username):
+        return render_template("auth/register.html", message = "Username already exists :(", 
+                                login=session.get("logged_in"), username = session.get("username"))
+    else:
+        db.execute(text("CALL sp_add_user(:username, :pwd)"),{"username": username, "pwd":password})
+        db.commit()
+        return render_template("auth/login.html", message = "Please sign in.",
+                                login = session.get("logged_in"), username = session.get("username"))
 
-#Search page for users to filter all products
+# When user forgets password and tries to reset
+@app.route("/forgot_password", methods=["GET", "POST"])
+def forgot_password():
+    return None
+
+# Search page for users to filter all products
 @app.route("/products", methods=["POST", "GET"])
 def products():
    product = get_all_products()
    return render_template("main/products.html", products=product, login=session.get("logged_in"), username=session.get("username"))
 
-# SUBROUTES of Products
-#Detailed information about the selected product
+## SUBROUTES of Products
+# Detailed information about the selected product
 @app.route("/products/product_id=<product_id>", methods=["GET"])
 def product(product_id):
-   return None
+   result = browse_one_products("WHERE product_id=%s" % product_id)
+   return render_template('main/product.html', product=result, login=session.get("logged_in"), username=session.get('username'))
 
-#Show all brands
+# Page to show all brands
 @app.route("/brands", methods=["POST", "GET"])
 def brands():
     brand = get_all_brands()
@@ -176,17 +197,30 @@ def brands():
         alphabets[brand_name[0].upper()].append(brand_name)
     return render_template("main/brands.html", brands=alphabets, login=session.get("logged_in"), username=session.get("username"))
 
-# SUBROUTES of Brands
-@app.route("/brand/brand_id=<brand_id>", methods=["GET"])
-def brand(brand_id):
-    return None
+## SUBROUTES of Brands
+# Page to show a brand and all of its products
+@app.route("/brand/brand_name=<brand_name>", methods=["GET"])
+def brand(brand_name = None):
+    prods = browse_products('WHERE brand_name=\'%s\''% brand_name)
+    return render_template("main/brand.html", products=prods, login=session.get("logged_in"), username=session.get("username"))
 
-#Adding product to shopping cart
-@app.route("/addToCart")
-def addToCart():
-    return None
+# Search by Product Type
+@app.route("/<product_type>", methods=['GET'])
+def product_type(product_type):
+    prods = browse_products('WHERE product_type=\'%s\'' % product_type)
+    return render_template("main/products.html", products=prods, login=session.get("logged_in"), username=session.get("username"))
 
-#Checking shopping cart
+# check shopping cart
 @app.route("/cart")
 def cart():
+    return None
+
+# check out items in your cart
+@app.route("/check_out")
+def check_out():
+    return None
+
+# my page to see order history, etc.
+@app.route("/mypage")
+def mypage():
     return None
